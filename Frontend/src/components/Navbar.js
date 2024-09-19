3294dc1de5cb18388678f651335a15c53ec0f7bb
ankit-0903 <1by22cs022@bmsit.in>
2024-09-19 17:31:18 +0530

    Updated css part

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -11,10 +11,10 @@ const Navbar = () => {
       <li><Link href="/">Home</Link></li>
         <li><Link href="/progress-classfication">Progress classfication</Link></li>
         <li><Link href="/3dmodel">3D Model</Link></li>
-        <li><Link href="/dashboard">Dashboard</Link></li>
         <li><Link href="/geo-tag-map">Geo Map</Link></li>
         <li><Link href="/workersafety">Worker Safety</Link></li>
         <li><Link href="/alerts">Alerts</Link></li>
+        <li><Link href="/dashboard">Dashboard</Link></li>
       </ul>
     </nav>
   );
